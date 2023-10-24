97f1ef99de606a10110f1269f12420c513e24bba
JanJaap <j-jschenk@hotmail.com>
2023-10-24 11:05:34 +0200

    added route for fetching individual plantcare

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Plant = require('../models/Plant');
-
+const mongoose = require("mongoose");
 
 router.get('/', (req, res, next) => {
   res.json('All good in here')
@@ -11,9 +11,28 @@ router.get("/plantcare", async (request, response) => {
   try {
     const plants = await Plant.find();
     response.status(200).json(plants);
-    console.log(plants)
   } catch (error) {
     response.status(500).json({ error: "Status code: 500 (Internal Server Error)" });
+  }
+});
+
+router.get("/plantcare/:plantCareId", async (request, response) => {
+  const { plantCareId } = request.params;
+  console.log(plantCareId)
+  if (mongoose.isValidObjectId(plantCareId)) {
+    try {
+      const currentPlantCare = await Plant.findById(plantCareId);
+      if (currentPlantCare) {
+        response.json({ student: currentPlantCare });
+      } else {
+        response.status(404).json({ message: "Plantcare not found" });
+      }
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error });
+    }
+  } else {
+    response.status(400).json({ message: "The id seems wrong" });
   }
 });
 
