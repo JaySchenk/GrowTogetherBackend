06a0cd17fa644ead89af2e04f98aae800f9c2f73
Kerem Karadag <akaradag32@gmail.com>
2023-10-30 21:01:25 +0100

    file upload and some fixes

--- models/UserPlants.model.js
+++ models/UserPlants.model.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-
 const userPlantSchema = new Schema({
-    plantname: {type: String, required: true},
-    plantSpecies: {type:mongoose.Schema.Types.ObjectId, ref: 'Plant'},
-    dateOfAcquisition: Date,
-    plantPicture: String,
-    plantCutting: {type: Number, default: 0},
-    plantSize: String,
-    productsUsed: [{
-        product: {
-          type: String,
-        },
-        dateUsed: {
-          type: Date,
-          default: Date.now
-        }
-      }],
-    careActivityDate: [{
-        activity: {
-          type: String,
-        },
-        dateOfCare: {
-          type: Date,
-          default: Date.now
-        }
-      }],
-      reminderSettings: Boolean  
+  plantName: { type: String, required: true },
+  plantSpecies: { type: mongoose.Schema.Types.ObjectId, ref: "Plant" },
+  dateOfAcquisition: Date,
+  plantPicture: String,
+  plantCutting: { type: Number, default: 0 },
+  plantSize: String,
+  productsUsed: [
+    {
+      product: {
+        type: String,
+      },
+      dateUsed: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  careActivityDate: [
+    {
+      activity: {
+        type: String,
+      },
+      dateOfCare: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  reminderSettings: Boolean,
 });
 
-const UserPlant = model('UserPlant', userPlantSchema);
+const UserPlant = model("UserPlant", userPlantSchema);
 
 module.exports = UserPlant;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Plant = require('../models/Plant.model');
-const UserPlant = require('../models/UserPlants.model')
-const User = require(`../models/User.model`)
+const Plant = require("../models/Plant.model");
+const UserPlant = require("../models/UserPlants.model");
+const User = require(`../models/User.model`);
 const mongoose = require("mongoose");
+const uploader = require("../middlewares/cloudinary.config.js");
 
-router.get('/', (req, res, next) => {
-  res.json('All good in here')
-})
+router.get("/", (req, res, next) => {
+  res.json("All good in here");
+});
 
 // get plantcare
 router.get("/plantcare", async (request, response) => {
@@ -15,7 +16,9 @@ router.get("/plantcare", async (request, response) => {
     const plants = await Plant.find();
     response.status(200).json(plants);
   } catch (error) {
-    response.status(500).json({ error: "Status code: 500 (Internal Server Error)" });
+    response
+      .status(500)
+      .json({ error: "Status code: 500 (Internal Server Error)" });
   }
 });
 // get individual plantcare
@@ -27,7 +30,9 @@ router.get("/plantcare/:plantCareId", async (request, response) => {
       if (currentPlantCare) {
         response.json({ Plant: currentPlantCare });
       } else {
-        response.status(404).json({ message: "Plantcare instructions not found" });
+        response
+          .status(404)
+          .json({ message: "Plantcare instructions not found" });
       }
     } catch (error) {
       console.log(error);
@@ -37,66 +42,78 @@ router.get("/plantcare/:plantCareId", async (request, response) => {
     response.status(400).json({ message: "The id seems wrong" });
   }
 });
-// get all users plants
 router.get("/userplants/:userId", async (request, response) => {
   const { userId } = request.params;
   try {
     const plants = await User.findById(userId).populate(`plants`);
     response.status(200).json(plants);
   } catch (error) {
-    response.status(500).json({ error: "Status code: 500 (Internal Server Error)" });
+    response
+      .status(500)
+      .json({ error: "Status code: 500 (Internal Server Error)" });
   }
 });
 // get 1 user plant
-router.get('/plant/:plantId', async (request, response) => {
+router.get("/plant/:plantId", async (request, response) => {
   const { plantId } = request.params;
   try {
-    const plant = await UserPlant.findById(plantId).populate('plantSpecies');
+    const plant = await UserPlant.findById(plantId).populate("plantSpecies");
     if (!plant) {
-      return response.status(404).json({ message: 'Plant not found' });
+      return response.status(404).json({ message: "Plant not found" });
     }
     response.status(200).json({ plant });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ error: 'Server error' });
+    response.status(500).json({ error: "Server error" });
   }
 });
 
 // post plantcare
-router.post('/plantcare', async (request, response) => {
+router.post("/plantcare", async (request, response) => {
   try {
-    const newPlantCare = await Plant.create(request.body)
-    response.status(201).json({ Plant: newPlantCare })
+    const newPlantCare = await Plant.create(request.body);
+    response.status(201).json({ Plant: newPlantCare });
   } catch (error) {
-    console.log(error)
-    response.status(400).json({ error })
+    console.log(error);
+    response.status(400).json({ error });
   }
-})
+});
 //post user plant
-router.post('/userplants/', async (request, response) => {
-  try {
-    const newUserPlant = await UserPlant.create(request.body)
-    response.status(201).json({ UserPlant: newUserPlant })
-  } catch (error) {
-    console.log(error)
-    response.status(400).json({ error })
+router.post(
+  "/userplants/",
+  uploader.single("plantPicture"),
+  async (request, response) => {
+    let payload = JSON.parse(request.body["Data"]);
+    payload.plantPicture = request.file.path;
+
+    try {
+      const newUserPlant = await UserPlant.create(payload);
+      response.status(201).json({ UserPlant: newUserPlant });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error });
+    }
   }
-})
+);
 
 // update existing plantcare
-router.put('/plantcare/:plantCareId', async (request, response) => {
+router.put("/plantcare/:plantCareId", async (request, response) => {
   const { plantCareId } = request.params;
 
   try {
-    const updatePlant = await Plant.findByIdAndUpdate(plantCareId, request.body, { new: true })
-    response.status(202).json({ Plant: updatePlant })
+    const updatePlant = await Plant.findByIdAndUpdate(
+      plantCareId,
+      request.body,
+      { new: true }
+    );
+    response.status(202).json({ Plant: updatePlant });
   } catch (error) {
-    console.log(error)
-    response.status(400).json({ error })
+    console.log(error);
+    response.status(400).json({ error });
   }
-})
+});
 // update user plant
-router.put('/userplants/:userPlantId', async (request, response) => {
+router.put("/userplants/:userPlantId", async (request, response) => {
   const { userPlantId } = request.params;
 
   try {
@@ -116,8 +133,8 @@ router.put('/userplants/:userPlantId', async (request, response) => {
     response.status(400).json({ error });
   }
 });
-//add user plant to user ID 
-router.put('/users/:UserId', async (request, response) => {
+//add user plant to user ID
+router.put("/users/:UserId", async (request, response) => {
   const { UserId } = request.params;
   const { plants } = request.body;
 
@@ -134,17 +151,17 @@ router.put('/users/:UserId', async (request, response) => {
   }
 });
 // update&replace user plant
-router.put('/userplantsUpdate/:userPlantId', async (request, response) => {
+router.put("/userplantsUpdate/:userPlantId", async (request, response) => {
   const { userPlantId } = request.params;
   const {
-    plantname,
+    plantName,
     plantSpecies,
     plantPicture,
     plantCutting,
     plantSize,
     reminderSettings,
     productsUsed,
-    careActivityDate
+    careActivityDate,
   } = request.body;
 
   try {
@@ -152,14 +169,14 @@ router.put('/userplantsUpdate/:userPlantId', async (request, response) => {
       userPlantId,
       {
         $set: {
-          plantname,
+          plantName,
           plantSpecies,
           plantPicture,
           plantCutting,
           plantSize,
           reminderSettings,
           productsUsed,
-          careActivityDate
+          careActivityDate,
         },
       },
       { new: true }
@@ -171,42 +188,44 @@ router.put('/userplantsUpdate/:userPlantId', async (request, response) => {
   }
 });
 // update user profile
-router.put('/userUpdate/:userId', async (request, response) => {
+router.put("/userUpdate/:userId", async (request, response) => {
   const { userId } = request.params;
-    try {
-    const updateUser = await User.findByIdAndUpdate(userId, request.body, { new: true });
+  try {
+    const updateUser = await User.findByIdAndUpdate(userId, request.body, {
+      new: true,
+    });
     response.status(202).json({ updatedUser: updateUser });
   } catch (error) {
     console.log(error);
-    response.status(400).json({ error: 'Failed to update user.' });
+    response.status(400).json({ error: "Failed to update user." });
   }
 });
 
 //get user profile
-router.get('/getUser/:userId', async (request, response) => {
+router.get("/getUser/:userId", async (request, response) => {
   const { userId } = request.params;
   try {
     const user = await User.findById(userId);
     if (!user) {
-      response.status(404).json({ error: 'User not found' });
+      response.status(404).json({ error: "User not found" });
     } else {
       response.status(200).json({ user });
     }
   } catch (error) {
     console.log(error);
-    response.status(500).json({ error: 'Failed to retrieve user.' });
+    response.status(500).json({ error: "Failed to retrieve user." });
   }
 });
 
 // delete plantcare
-router.delete('/plantcare/:plantCareId', async (request, response) => {
-  const { plantCareId } = request.params
+router.delete("/plantcare/:plantCareId", async (request, response) => {
+  const { plantCareId } = request.params;
 
-  await Plant.findByIdAndDelete(plantCareId)
-  response.status(202).json({ message: 'Plant deleted' })
-})
+  await Plant.findByIdAndDelete(plantCareId);
+  response.status(202).json({ message: "Plant deleted" });
+});
 // delete user plant
-router.delete('/userplants/:userId/:userPlantId', async (request, response) => {
+router.delete("/userplants/:userId/:userPlantId", async (request, response) => {
   const { userId, userPlantId } = request.params;
 
   try {
@@ -217,13 +236,13 @@ router.delete('/userplants/:userId/:userPlantId', async (request, response) => {
     );
     await UserPlant.findByIdAndDelete(userPlantId);
 
-    response.status(202).json({ message: 'Plant deleted' });
+    response.status(202).json({ message: "Plant deleted" });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ error: 'Server error' });
+    response.status(500).json({ error: "Server error" });
   }
 });
 
 /* Make some routes there for what you need, don't forget that you can use your middleware where you define the use of this router */
 
-module.exports = router
+module.exports = router;
